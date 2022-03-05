7f67e09f699d30f14ff0a19a0e3baf198080bd3f
Dustin Pfannenstiel <dmp@hey.com>
2022-03-04 19:46:33 -0600

    Initial Commit

---
+++ Shared/WeSplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplitApp.swift
+//  Shared
+//
+//  Created by Dustin Pfannenstiel on 3/4/22.
+//
+
+import SwiftUI
+
+@main
+struct WeSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
