073b6fcbb9c3ded676597deed6ef878862479a33
Dustin Pfannenstiel <dmp@hey.com>
2022-03-06 13:03:17 -0600

    Added Guess The Flag Paul's way

---
+++ GuessTheFlag/Shared/GuessTheFlagApp.swift
@@ -0,0 +1,17 @@
+//
+//  GuessTheFlagApp.swift
+//  Shared
+//
+//  Created by Dustin Pfannenstiel on 3/6/22.
+//
+
+import SwiftUI
+
+@main
+struct GuessTheFlagApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
